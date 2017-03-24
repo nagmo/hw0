99e8d0f6904bd47515f07ba9cf0d31b4f9f6e3a9
nagmo <nevo@h-MacBook-Pro-sl-Nevo.local>
2017-03-24 11:19:04 +0300

    Initial Commit

---
+++ hw0/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hw0
+//
+//  Created by Nevo Agmon on 3/24/17.
+//  Copyright © 2017 Nevo Agmon. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
